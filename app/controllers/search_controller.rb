bd2b3d87f91e0891262baa0e5795846e4a17ad15
Fenton Taylor <fentontaylor@gmail.com>
2019-10-14 10:20:30 -0600

    Refactor to use PotterApiService

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,22 +1,8 @@
 class SearchController < ApplicationController
   def index
     @house = params[:house]
-    @members = get_members(@house)
-  end
+    @members = PotterApiService.get_members(@house)
 
-  private
-
-  def get_members(house)
-    conn = Faraday.new('https://www.potterapi.com/v1/')
-    response = conn.get do |req|
-      req.url 'characters'
-      req.params['key'] = ENV['API_KEY']
-      req.params['house'] = house
-      req.params['orderOfThePhoenix'] = true
-    end
-
-    json = JSON.parse(response.body)
-
-    json.map {|char| Character.new(char)}
+    @facade = SearchFacade.new(params[:house])
   end
 end
---
+++ app/services/potter_api_service.rb
@@ -0,0 +1,26 @@
+class PotterApiService
+  def self.get_members(house)
+    PotterApiService.new.get_members(house)
+  end
+
+  def get_members(house)
+    response = phoenix_members(house)
+    json = JSON.parse(response.body)
+    json.map {|char| Character.new(char)}
+  end
+
+  private
+
+  def conn
+    Faraday.new('https://www.potterapi.com/v1/')
+  end
+
+  def phoenix_members(house)
+    conn.get do |req|
+      req.url 'characters'
+      req.params['key'] = ENV['API_KEY']
+      req.params['house'] = house
+      req.params['orderOfThePhoenix'] = true
+    end
+  end
+end
