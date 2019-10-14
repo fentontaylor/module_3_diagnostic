3b2440b0cda7d4406069fe7930000b182e52dc07
Fenton Taylor <fentontaylor@gmail.com>
2019-10-14 11:15:45 -0600

    Add service test for PotterApiService

--- spec/features/search/user_selects_house_from_menu_spec.rb
+++ spec/features/search/user_selects_house_from_menu_spec.rb
@@ -1,18 +1,4 @@
 require 'rails_helper'
-# As a user,
-# When I visit "/"
-# And I Select "Griffindor" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members"
-# Then I should be on page "/search"
-# Then I should see a total of the number of members for that house. (18 for Griffindor)
-# Then I should see a list of 21 members
-#
-# And for each of the members I should see:
-# - The name of the member
-# - The members role (if it exists)
-# - The house the member belongs to
-# - The Patronus of the member (if it exists)
 
 describe 'As a user' do
   describe 'I select a house and click "Search For Members"' do
--- spec/services/potter_api_service_spec.rb
+++ spec/services/potter_api_service_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 describe 'PotterApiService' do
-  it 'returns a ' do
-    
+  context '#get_members' do
+    it 'returns a parsed json hash of Phoenix members for a house' do
+      members = File.open('./fixtures/phoenix_members_gryffindor.json')
+      url = "https://www.potterapi.com/v1/characters?key=$2a$10$JBNoRo7G/4R.8KaIfDDqk.0i6.2llWK97mSxws2ozodD57i9EXHvW&orderOfThePhoenix=true&house=Gryffindor"
+      stub_request(:get, url).to_return(status: 200, body: members)
+
+      members = PotterApiService.new.get_members('Gryffindor')
+
+      expect(members).to be_an Array
+      expect(members.count).to eq(21)
+
+      sirius = members.first
+      expect(sirius['name']).to eq('Sirius Black')
+      expect(sirius['house']).to eq('Gryffindor')
+      expect(sirius['role']).to be nil
+      expect(sirius['patronus']).to be nil
+
+      abe = members.second
+      expect(abe['name']).to eq('Aberforth Dumbledore')
+      expect(abe['house']).to eq('Gryffindor')
+      expect(abe['role']).to eq("Owner, Hog's Head Inn")
+      expect(abe['patronus']).to eq('goat')
+    end
   end
 end
